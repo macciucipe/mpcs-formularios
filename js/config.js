b6a580f389c04afdbb91033312edb1d1e097560f
macciucipe <admin@mpcs.pe>
2026-05-30 18:16:22 -0500

    config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = 'https://zaurhkkcksqsaghpegdn.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphdXJoa2tja3Nxc2FnaHBlZ2RuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NTIyMDIsImV4cCI6MjA2NDEyODIwMn0.s9a3oBkMNLFBEbJLQWsY8TgfHPi8FN7qJJxrOCxKaGQ';
+const SUPABASE_ANON_KEY = 'sb_publishable_evqKFHcvSVCLgAtwcHyQaA_hY-4iAXK';
 
 const sbClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
